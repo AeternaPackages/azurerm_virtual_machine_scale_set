38b654a74278c331754c63f6c3b46b26e20d8c6d
Daniel Allen <dan@aeternagroup.net>
2026-09-03 11:41:05 +0100

    Update to schema version v5.0.1

--- main.tf
+++ main.tf
@@ -21,18 +21,18 @@ locals {
 }
 
 module "virtual_machine_scale_sets" {
-  source                     = "git::https://github.com/AeternaModules/azurerm_virtual_machine_scale_set.git?ref=v5.0.0"
+  source                     = "git::https://github.com/AeternaModules/azurerm_virtual_machine_scale_set.git?ref=v5.0.1"
   virtual_machine_scale_sets = local.virtual_machine_scale_sets
 }
 
 module "virtual_machine_scale_set_extensions" {
-  source                               = "git::https://github.com/AeternaModules/azurerm_virtual_machine_scale_set_extension.git?ref=v5.0.0"
+  source                               = "git::https://github.com/AeternaModules/azurerm_virtual_machine_scale_set_extension.git?ref=v5.0.1"
   virtual_machine_scale_set_extensions = local.virtual_machine_scale_set_extensions
   depends_on                           = [module.virtual_machine_scale_sets]
 }
 
 module "virtual_machine_scale_set_packet_captures" {
-  source                                    = "git::https://github.com/AeternaModules/azurerm_virtual_machine_scale_set_packet_capture.git?ref=v5.0.0"
+  source                                    = "git::https://github.com/AeternaModules/azurerm_virtual_machine_scale_set_packet_capture.git?ref=v5.0.1"
   virtual_machine_scale_set_packet_captures = local.virtual_machine_scale_set_packet_captures
   depends_on                                = [module.virtual_machine_scale_sets]
 }
